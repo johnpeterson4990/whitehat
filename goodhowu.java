c95a4e35186f9a5f9a33a3171a381d89541dde86
petersonalderson <63938551+petersonalderson@users.noreply.github.com>
2020-04-18 19:05:32 -0700

    Add files via upload

---
+++ goodhowu.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
+public class goodhowu 
+{
+   public static void main(String [] args)
+  {
+      {
+      int i = 0;
+      Scanner input = new Scanner(System.in);
+      System.out.println("Hi, how are you?");
+      for ( ; ; )
+         {
+         if (i==1)
+            {
+            break;
+            }      
+            for (int k=1; k<2; k++)
+               {
+               String text = input.next();
+               System.out.println("Good, how are you?");
+               }        
+         }  
+      }
+   }
+}     
